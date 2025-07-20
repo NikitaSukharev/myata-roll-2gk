a67e259903300e940ed8c9b53210b2e27377319e
Nikita Sukharev <niita5505@icloud.com>
2025-07-21 02:15:28 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,16 +13,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["example.com"],
-  },
-  webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve(__dirname);
-    return config;
-  },
-};
-
-module.exports = nextConfig;
